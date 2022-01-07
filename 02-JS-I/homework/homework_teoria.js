b9e7dede8cac65390c92bce435bcfcbaa836c613
Juan Pablo Rodriguez <jprp_2403@live.com>
2022-01-07 14:51:50 -0500

    realizado

---
+++ 02-JS-I/homework/homework_teoria.js
@@ -0,0 +1,25 @@
+// Que es una variable?
+
+//Es una manera de guardar informacion para luego usarla u hacer algo con ella y pueden ser cualquier cosa
+//respetando las normas
+
+//Que es un string?
+
+//un string o cadena de texto, es una sucesion de letras o numeros u caracteres en general
+
+//Funciones
+
+//Una funcion se puede entender como una actividad tal como jugar al futbol y los argumentos
+// como los equipos que van a jugar o la cantidad de personas que conformaran los equipos
+// y la declaracion de "return" se usa para saber el marcador del partido y si no se usa
+// no se podria saber el marcador final
+
+//Que son las declaraciones?
+
+//se usa para elegir entre varias opciones por ejemplo, si la mayoria quiere jugar basquet y si no, pues futbol
+//de una manera literal se puede entender como, si esto es verdad esto otro pasara y no, pues no
+
+//Que son los valores booleanos?
+
+//se puede entender como si algo es verdad o es mentira, tal que tienes 12 años? pues si evidentemente
+// o "eres mayor de edad?" pues evidentemente es falso ya que no tienes mas de 18 años
---
+++ 03-JS-II/homework/homework_teoria.js
@@ -0,0 +1,25 @@
+// Que es for?
+
+//Este es utilizado para repetir un bloque de codigo mientras sea "true", por poner
+//un ejemplo cuando tu madre te decia que te comieras toda la comida, ella te dira eso
+//hasta que veas que comiste todo, y esta expresion ya es falsa porque ya te comiste todo
+// puedes irte a jugar o lo que sea
+
+//Que es &&?
+
+//Se utiliza para comprobar que dos condicionales sean verdaderas y se ejecutara solamente 
+//si ambas son verdaderas, por ejemplo, si tenemos 2 colores para conseguir morado, unicamente 
+//obtendremos morado cuando sean rojo y azul, si son otros colores no podremos hacer el morado
+
+//Que es ||?
+
+//Esta se entiende como un "y" o "and", la diferencia del "&&" es que en este, solamente se necesita 
+//que una de las condicionales sea verdadera,por ejemplo si nos van a dar dos colores al azar
+// y establecemos que si alguno de esos dos es morado o rojo, y resulta que el primero fue efectivamente
+//morado a pesar de que el otro haya sido amarillo, el bloque de codigo o la funcion correra
+
+//Que es !?
+
+//Se entiende como un "NO" o tambien se usa para diferenciar un algo de otra cosa, por ejemplo
+// cuando es "!=" es como decir diferente de, por ejemplo esta pelota de basquet es diferente 
+//de una pelota de futbol
